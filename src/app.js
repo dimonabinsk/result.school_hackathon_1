de2322d73eabeb2636f04c558eedaecfb24d1096
Julia <shapareva.ju@gmail.com>
2022-08-27 00:11:05 +0300

    adding module with changing background

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import './styles/styles.css';
+import { BackgroundModule } from '../src/modules/background.module.js'
 
+const elBackground = document.querySelector ('[data-type = backgroundModule]')
 
-
-
+elBackground?.addEventListener ('click', () => {
+let element = new BackgroundModule ("backgroundModule", "Изменить фон")
+element.trigger()
+})
