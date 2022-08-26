fe675cc22e88b4f724591bb34b6f26b39199ea3b
dimonabinsk <dimonabinsk@yandex.ru>
2022-08-27 02:46:12 +0300

    предварительная логика

--- src/app.js
+++ src/app.js
@@ -1,61 +1,7 @@
 import './styles/styles.css';
+import {ContextMenu} from "./menu";
 
-const body = document.body;
-
-const arrItem = [
-	"Аналитика кликов",
-	"Случайная фигура",
-	"Таймер отсчета",
-	"Случайный звук",
-	"Случайный фон",
-	"Кастомное сообщение"
-];
-
-function createListItem(arr) {
-	return arr.map(str => {
-		const itemLiHTML = document.createElement("li");
-		itemLiHTML.className = "menu-item";
-		itemLiHTML.textContent = str;
-		return itemLiHTML;
-	})
-}
+const contextMenu = new ContextMenu(".menu");
+contextMenu.open();
 
 
-body.addEventListener("contextmenu", (e) => {
-	e.preventDefault();
-	// console.log(e.pageY, e.pageX);
-	const h = e.pageY;
-	const w = e.pageX;
-	const bodyWidth = body.clientWidth;
-	console.log("bodyWidth", bodyWidth)
-	console.log("w", w)
-
-	const bodyHeight = body.clientHeight;
-	console.log("bodyHeight", bodyHeight)
-	console.log("h",h)
-const elemItem = createListItem(arrItem);
-	// console.log(elemItem)
-
-	const listMenu = document.querySelector(".menu");
-	listMenu.innerHTML = "";
-	elemItem.forEach(elem => listMenu.append(elem));
-	const offsetHeight = listMenu.offsetHeight;
-	const offsetWidth = listMenu.offsetWidth;
-	console.log("offsetWidth", offsetWidth);
-
-	if(h < offsetHeight || w < offsetWidth &&  bodyWidth > w + offsetWidth) {
-		listMenu.style.left = `${w}px`;
-		listMenu.style.top = `${h}px`;
-	}
-	if(h < offsetHeight && w < offsetWidth &&  bodyWidth < w + offsetWidth) {
-		listMenu.style.left = `${w - offsetWidth}px`;
-		listMenu.style.top = `${h}px`;
-	}
-	else {
-		listMenu.style.left = `${w - offsetWidth}px`;
-		listMenu.style.top = `${h - offsetHeight}px`;
-
-	}
-	listMenu.classList.add("open");
-
-})
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,49 @@
-import { Menu } from './core/menu'
+import {Menu} from './core/menu'
 
-export class ContextMenu extends Menu
-{
+export class ContextMenu extends Menu {
+	#arr;
+	#body;
+	#itemHTML;
+	constructor(selector) {
+		super(selector);
+		this.#body  = document.body;
+		this.#arr = [
+			"Аналитика кликов",
+			"Случайная фигура",
+			"Таймер отсчета",
+			"Случайный звук",
+			"Случайный фон",
+			"Кастомное сообщение"
+		];
+
+		this.#itemHTML = this.#arr.map(str => {
+			const itemLiHTML = document.createElement("li");
+			itemLiHTML.className = "menu-item";
+			itemLiHTML.textContent = str;
+			return itemLiHTML;
+		})
+
+	}
+
+	open() {
+		this.#body.addEventListener("contextmenu", (e) => {
+			e.preventDefault();
+			this.close();
+			this.add();
+			this.el.style.left = `${e.offsetX}px`;
+			this.el.style.top = `${e.offsetY}px`;
+			this.el.classList.add("open");
+		});
+	}
+
+	add() {
+		this.#itemHTML.forEach(elem => this.el.append(elem));
+	}
+
+	close() {
+		this.el.innerHTML = "";
+		this.el.classList.remove("open");
+	}
+
 
 }
