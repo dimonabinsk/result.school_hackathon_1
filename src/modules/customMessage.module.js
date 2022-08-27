46edd6956cf94d0f46e64bed1a6ec5c63f02425c
Axent13 <axent13@yandex.ru>
2022-08-27 14:00:41 +0700

    Сделал  customMesage

--- src/modules/customMessage.module.js
+++ src/modules/customMessage.module.js
@@ -1,5 +1,36 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { random } from '../utils/utils';
+
+const messages = [
+  'Это случайное сообщение',
+  'I love JavaScript',
+  '42',
+  'Здесь могла бы быть ваша реклама',
+  'НЛО прилетело и опубликовало эту надпись здесь'
+];
 
 export class CustomMessageModule extends Module {
+  #messagesContainerHTML;
 
+  constructor(type, text) {
+    super(type, text);
+    this.#messagesContainerHTML = document.createElement('div');
+    this.#messagesContainerHTML.classList.add('custom-messages');    
+    document.body.append(this.#messagesContainerHTML);
+  }
+
+  trigger() {
+    const newMessage = document.createElement('p');
+    newMessage.classList.add('custom-messages__message');
+    newMessage.textContent = messages[random(0, messages.length - 1)];
+
+    setTimeout(() => {
+      newMessage.classList.add('custom-messages__message_hidden');
+    }, 3000);
+    setTimeout(() => {
+      newMessage.remove();
+    }, 3500);
+
+    this.#messagesContainerHTML.append(newMessage);
+  }
 }
