f980eea38b5027716cc1f4b31aadc418b2c2063d
dimonabinsk <dimonabinsk@yandex.ru>
2022-08-27 05:04:15 +0300

    удалил элементы из app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
 import './styles/styles.css';
-import { BackgroundModule } from './modules/background.module';
-
-const elBackground = document.querySelector ('[data-type = "random-background"]')
-
-elBackground?.addEventListener ('click', () => {
-let element = new BackgroundModule ("backgroundModule", "Изменить фон")
-element.trigger()
-})
