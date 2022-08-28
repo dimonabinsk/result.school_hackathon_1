55149372e94b0394ef96057901f87d507e104d14
dimonabinsk <dimonabinsk@yandex.ru>
2022-08-28 18:32:01 +0300

    реализовал аналитика кликов

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -5,26 +5,18 @@ export class ClicksModule extends Module {
 	#container;
 	#countdown;
 	#label;
-	#count;
-	#counter;
 
 	constructor(type, text) {
 		super(type, text);
 		this.#body = document.body;
-		this.#counter = 0;
-
 	}
 
 
 	trigger() {
 		this.clearBody();
-		this.#count = 5;
-		this.#counter = 0;
 		this.#container = document.createElement("div");
-		this.#container.className = "container-countdown";
 		this.#label = document.createElement("span");
 		this.#countdown = document.createElement("span");
-		this.#countdown.className = "countdown-span";
 		this.#container.append(this.#label, this.#countdown);
 		this.#body.append(this.#container);
 		this.#timerCountDown();
@@ -32,26 +24,25 @@ export class ClicksModule extends Module {
 	}
 
 	#timerCountDown() {
-		this.#click();
+	let counter = 0;
+	let time = 5;
 		const startTimer = setInterval(() => {
-			this.#label.textContent = "Время пошло, давай кликай по мне ";
-			this.#countdown.textContent = `0${this.#count}`;
-			this.#count--;
-			if(this.#count < 0) {
+			this.#label.textContent = "Кликай по экрану, время пошло:  ";
+			this.#countdown.textContent = ` 0${time} сек`;
+			time--;
+			if(time < 0) {
 				clearInterval(startTimer);
 				this.#label.textContent = "Время вышло, количество кликов: ";
-				this.#countdown.textContent = `${this.#click()} раз`;
+				this.#countdown.textContent = `${counter} раз`;
 			}
-		}, 1000)
-	}
+		}, 1000);
 
-	#click() {
 		const upCounter = () => {
-			this.#counter++;
+			counter++;
 		}
 		this.#body.addEventListener("click", upCounter);
 		this.#body.addEventListener("dblclick", upCounter);
 
-		return this.#counter;
 	}
+
 }
