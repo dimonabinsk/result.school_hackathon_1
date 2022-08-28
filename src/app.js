0ec281c02b85704f35943e3b343b601634bd8773
dimonabinsk <dimonabinsk@yandex.ru>
2022-08-28 10:00:35 +0300

    удалил ненужный массив из app.js

--- src/app.js
+++ src/app.js
@@ -9,33 +9,6 @@ import {TimerModule} from "./modules/timer.module";
 import { PokemonModule } from './modules/pokemon.module';
 
 
-// const tasks = [
-// 	{
-// 		name:"Аналитика кликов",
-// 		data:"click-analytics",
-// 	},
-// 	{
-// 		name:"Случайная фигура",
-// 		data:"random-figure",
-// 	},
-// 	{
-// 		name:"Таймер отсчета",
-// 		data:"countdown-timer",
-// 	},
-// 	{
-// 		name:"Случайный звук",
-// 		data:"random-sound",
-// 	},
-// 	{
-// 		name:"Случайный фон",
-// 		data:"random-background",
-// 	},
-// 	{
-// 		name:"Кастомное сообщение",
-// 		data:"custom-message",
-// 	}
-//
-// ];
 
 const modules = [
 	new BackgroundModule('random-background', 'Случайный фон'),
