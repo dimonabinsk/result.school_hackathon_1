590a767d4bd6c0ecae256f259db52f27b7c4b337
dimonabinsk <dimonabinsk@yandex.ru>
2022-08-28 22:29:52 +0300

    Update shape.module.js

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -15,18 +15,16 @@ export class ShapeModule extends Module {
 		elem.style.backgroundColor = `rgb(${random(10, 230)},${random(90, 200)},${random(30, 210)})`;
 	}
 
-
-
 	trigger() {
 		this.clearBody();
 		const {width, height} = this.#body.getBoundingClientRect();
-		const sizeCircle = random(25, 100);
-		const x = random(0, width - sizeCircle);
-		const y = random(0, height - sizeCircle);
+		const sizeFigure = random(25, 100);
+		const x = random(0, width - sizeFigure);
+		const y = random(0, height - sizeFigure);
 		this.#body.style.position = "relative";
 		this.#figure.style.position = "absolute";
-		this.#figure.style.width = `${sizeCircle}px`;
-		this.#figure.style.height = `${sizeCircle}px`;
+		this.#figure.style.width = `${sizeFigure}px`;
+		this.#figure.style.height = `${sizeFigure}px`;
 		this.#figure.style.top = `${y}px`;
 		this.#figure.style.left = `${x}px`;
 		this.#figure.style.borderRadius = `${random(0, 50)}%`;
